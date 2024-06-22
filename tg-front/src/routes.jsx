883547d4de5ca55a4024c748a079f4d33d54e846
Alexander Shelyugov <ashelyugov@noreply.email>
2024-06-22 11:49:01 +0200

    Prepare loading initial data

--- tg-front/src/routes.jsx
+++ tg-front/src/routes.jsx
@@ -1,15 +1,24 @@
 import { createBrowserRouter } from "react-router-dom"
 
 import App from "@/App"
+
+import { getStatistics } from "@/features/statistics/api/StatisticsApi"
+
 import WelcomePage from "@/features/registration/pages/WelcomePage"
 import WizardPage from "@/features/registration/pages/WizardPage"
 import SignupCompletePage from "@/features/registration/pages/SignupCompletedPage"
 import StatisticsPage from "@/features/statistics/pages/StatisticsPage"
 
+async function rootLoader() {
+    const statistics = await getStatistics()
+    return statistics
+}
+
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        // loader: rootLoader,
         children: [
             { index: true, element: <WelcomePage /> },
             {
