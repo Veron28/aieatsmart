cc464cb4e2b9ac33f0f7b681da47705e55f9e9dc
Alexander Shelyugov <ashelyugov@noreply.email>
2024-07-05 19:06:12 +0200

    Refactor styles a little

--- tg-front/src/index.jsx
+++ tg-front/src/index.jsx
@@ -1,13 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
-import "./index.css"
 
 import router from "./routes"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(
-    <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
         <RouterProvider router={router} />
-    </React.StrictMode>
+    </StrictMode>
 )
