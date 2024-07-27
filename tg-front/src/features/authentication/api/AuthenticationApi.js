288138c91d20e25a502a5d416acac695f2985726
Alexander Shelyugov <ashelyugov@noreply.email>
2024-07-28 01:13:51 +0200

    Bring auth back

--- tg-front/src/features/authentication/api/AuthenticationApi.js
+++ tg-front/src/features/authentication/api/AuthenticationApi.js
@@ -1,6 +1,6 @@
 import { callBackend } from "@/api/client"
 
-export const isAuthenticatedFn = (userData) => false // userData?.is_stat ?? false
+export const isAuthenticatedFn = (userData) => userData?.is_stat ?? false
 
 export function getUserData() {
     return callBackend("/api/v1/auth", { method: "POST" })
